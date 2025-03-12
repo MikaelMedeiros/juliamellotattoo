815b51fd3a6ac02b199eb136f51de0192333600c
Mikael Medeiros <mikaeltruth@gmail.com>
2025-03-12 20:50:24 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { MenuSiteComponent } from "./menu-site/menu-site.component";
+import { BannerSiteComponent } from "./banner-site/banner-site.component";
+import { BioSiteComponent } from "./bio-site/bio-site.component";
+import { PortfolioSiteComponent } from "./portfolio-site/portfolio-site.component";
+import { FooterSiteComponent } from "./footer-site/footer-site.component";
+import { FormSiteComponent } from "./form-site/form-site.component";
+import { TipsSiteComponent } from "./tips-site/tips-site.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ButtonModule, MenuSiteComponent, BannerSiteComponent, BioSiteComponent, PortfolioSiteComponent, FooterSiteComponent, FormSiteComponent, TipsSiteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,47 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { providePrimeNG } from 'primeng/config';
 import { provideRouter } from '@angular/router';
+import { definePreset } from '@primeng/themes';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 
+const JuPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+        50:  '{gray.50}',
+        100: '{gray.100}',
+        200: '{gray.200}',
+        300: '{gray.300}',
+        400: '{gray.400}',
+        500: '{gray.500}',
+        600: '{gray.600}',
+        700: '{gray.700}',
+        800: '{gray.800}',
+        900: '{gray.900}',
+        950: '{gray.950}'
+    }
+}
+});
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideAnimations(),
+    providePrimeNG({
+      theme: {
+        preset: JuPreset,
+        options: {
+            cssLayer: {
+                name: 'primeng',
+                order: 'theme, base, primeng'
+            }
+        }
+      } 
+    })
+  ]
 };
